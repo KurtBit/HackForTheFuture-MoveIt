b7017293c031bcdd3d83ec69cd489109e7e40318
Dominent <petromilpavlov@gmail.com>
2016-10-01 20:02:29 +0300

    Renamed WebUi.Models to WebUi.ViewModels

---
+++ MoveIt.WebUi/Controllers/GameLobby.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MoveIt.WebUi.Controllers
+{
+    public class GameLobby : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
