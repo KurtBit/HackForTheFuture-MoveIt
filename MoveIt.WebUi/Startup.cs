48c083a7bf7c25ac542742cc6cca3d3a23273b6a
Dominent <petromilpavlov@gmail.com>
2016-10-01 10:30:48 +0300

    Add Basic Structure.

---
+++ MoveIt.WebUi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoveIt.WebUi.Startup))]
+namespace MoveIt.WebUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
